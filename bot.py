b3d30e9a6c4865cf57db575a44c907ec5141b9e0
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2019-10-12 18:16:18 +0200

    added function send_cat_picture

--- bot.py
+++ bot.py
@@ -1,12 +1,16 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from glob import glob
 import logging
-import settings
+from random import choice
 
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+import settings
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
                     filename='bot.log'
                     )
+
 
 def greet_user(bot, update):
     text = 'Вызван /start'
@@ -15,26 +19,31 @@ def greet_user(bot, update):
 
 
 def talk_to_me(bot, update):
-    user_text = 'Привет {}! Ты написал: {}'.format(update.message.chat.first_name, update.message.text) 
-    logging.info('User: %s, Chat id: %s, Message: %s', update.message.chat.username, 
-                                                        update.message.chat.id, 
+    user_text = 'Привет {}! Ты написал: {}'.format(update.message.chat.first_name, update.message.text)
+    logging.info('User: %s, Chat id: %s, Message: %s', update.message.chat.username,
+                                                        update.message.chat.id,
                                                         update.message.text)
     update.message.reply_text(user_text)
 
 
+def send_cat_picture(bot, update):
+    cat_list = glob('images/cat*.jp*g')
+    cat_pic = choice(cat_list)
+    bot.send_photo(chat_id=update.message.chat.id, photo=open(cat_pic, 'rb'))
+
+
 def main():
-    mybot = Updater(settings.API_KEY, request_kwargs=settings.PROXY)
+    mybot = Updater(settings.API_KEY)
 
     logging.info('Бот запускается')
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("cat", send_cat_picture))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
-
 
     mybot.start_polling()
     mybot.idle()
 
 
 main()
-
