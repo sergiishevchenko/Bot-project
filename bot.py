5560f422bee064d37148b8a5979626a2e15574e0
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2019-10-12 19:48:17 +0200

    added function get_user_emo

--- bot.py
+++ bot.py
@@ -2,7 +2,9 @@ from glob import glob
 import logging
 from random import choice
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from emoji import emojize
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, RegexHandler
 
 import settings
 
@@ -12,24 +14,36 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
                     )
 
 
-def greet_user(bot, update):
-    text = 'Вызван /start'
+def greet_user(bot, update, user_data):
+    emo = get_user_emo(user_data)
+    user_data['emo'] = emo
+    text = 'Привет {}'.format(emo)
+    my_keyboard = ReplyKeyboardMarkup([['Прислать котика', 'Сменить аватарку']])
     logging.info(text)
-    update.message.reply_text(text)
+    update.message.reply_text(text, reply_markup=my_keyboard)
 
 
-def talk_to_me(bot, update):
-    user_text = 'Привет {}! Ты написал: {}'.format(update.message.chat.first_name, update.message.text)
+def talk_to_me(bot, update, user_data):
+    emo = get_user_emo(user_data)
+    user_text = 'Привет {} {}! Ты написал: {}'.format(update.message.chat.first_name, emo, update.message.text)
     logging.info('User: %s, Chat id: %s, Message: %s', update.message.chat.username,
                                                         update.message.chat.id,
                                                         update.message.text)
     update.message.reply_text(user_text)
 
 
-def send_cat_picture(bot, update):
+def send_cat_picture(bot, update, user_data):
     cat_list = glob('images/cat*.jp*g')
     cat_pic = choice(cat_list)
     bot.send_photo(chat_id=update.message.chat.id, photo=open(cat_pic, 'rb'))
+
+
+def get_user_emo(user_data):
+    if 'emo' in user_data:
+        return user_data['emo']
+    else:
+        user_data['emo'] = emojize(choice(settings.USER_EMOJI), use_aliases=True)
+        return user_data['emo']
 
 
 def main():
@@ -38,9 +52,11 @@ def main():
     logging.info('Бот запускается')
 
     dp = mybot.dispatcher
-    dp.add_handler(CommandHandler("start", greet_user))
-    dp.add_handler(CommandHandler("cat", send_cat_picture))
-    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    dp.add_handler(CommandHandler("start", greet_user, pass_user_data=True))
+    dp.add_handler(CommandHandler("cat", send_cat_picture, pass_user_data=True))
+    dp.add_handler(RegexHandler('^(Прислать котика)$', send_cat_picture, pass_user_data=True))
+    dp.add_handler(RegexHandler('^(Сменить аватарку)$', change_avatar, pass_user_data=True))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me, pass_user_data=True))
 
     mybot.start_polling()
     mybot.idle()
