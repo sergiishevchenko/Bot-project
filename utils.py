7cfcec0404668596f78753f8c34e3bef8e5313bb
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2019-10-13 17:56:32 +0200

    first commit

---
+++ utils.py
@@ -0,0 +1,20 @@
+from emoji import emojize
+from random import choice
+import settings
+from telegram import ReplyKeyboardMarkup, KeyboardButton
+
+
+def get_user_emo(user_data):
+    if 'emo' in user_data:
+        return user_data['emo']
+    else:
+        user_data['emo'] = emojize(choice(settings.USER_EMOJI), use_aliases=True)
+        return user_data['emo']
+
+
+def get_keyboard():
+    contact_button = KeyboardButton('Контактные данные', request_contact=True)
+    location_button = KeyboardButton('Геолокация', request_location=True)
+    my_keyboard = ReplyKeyboardMarkup([['Прислать котика', 'Сменить аватарку'], [contact_button, location_button]], resize_keyboard=True)
+
+    return my_keyboard
