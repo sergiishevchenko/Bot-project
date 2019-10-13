e9d5744c3d0d5817b87d0d4f8b6cd9f6d3c517cd
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2019-10-13 16:05:31 +0200

    added functions change_avatar, get_contact, get_location, get_keyboard

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import logging
 from random import choice
 
 from emoji import emojize
-from telegram import ReplyKeyboardMarkup
+from telegram import ReplyKeyboardMarkup, KeyboardButton
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, RegexHandler
 
 import settings
@@ -18,7 +18,9 @@ def greet_user(bot, update, user_data):
     emo = get_user_emo(user_data)
     user_data['emo'] = emo
     text = 'Привет {}'.format(emo)
-    my_keyboard = ReplyKeyboardMarkup([['Прислать котика', 'Сменить аватарку']])
+    contact_button = KeyboardButton('Контактные данные', request_contact=True)
+    location_button = KeyboardButton('Геолокация', request_location=True)
+    my_keyboard = ReplyKeyboardMarkup([['Прислать котика', 'Сменить аватарку'], [contact_button, location_button]])
     logging.info(text)
     update.message.reply_text(text, reply_markup=my_keyboard)
 
@@ -46,6 +48,31 @@ def get_user_emo(user_data):
         return user_data['emo']
 
 
+def change_avatar(bot, update, user_data):
+    if 'emo' in user_data:
+        del user_data['emo']
+    emo = get_user_emo(user_data)
+    update.message.reply_text('Готово: {}'.format(emo))
+
+
+def get_contact(bot, update, user_data):
+    print(update.message.contact)
+    update.message.reply_text('Готово {}'.format(get_user_emo(user_data)))
+
+
+def get_location(bot, update, user_data):
+    print(update.message.location)
+    update.message.reply_text('Спасибо {}'.format(get_user_emo(user_data)))
+
+
+def get_keyboard():
+    contact_button = KeyboardButton('Контактные данные', request_contact=True)
+    location_button = KeyboardButton('Геолокация', request_location=True)
+    my_keyboard = ReplyKeyboardMarkup([['Прислать котика', 'Сменить аватарку'], [contact_button, location_button]], resize_keyboard=True)
+
+    return my_keyboard
+
+
 def main():
     mybot = Updater(settings.API_KEY)
 
@@ -56,6 +83,9 @@ def main():
     dp.add_handler(CommandHandler("cat", send_cat_picture, pass_user_data=True))
     dp.add_handler(RegexHandler('^(Прислать котика)$', send_cat_picture, pass_user_data=True))
     dp.add_handler(RegexHandler('^(Сменить аватарку)$', change_avatar, pass_user_data=True))
+    dp.add_handler(MessageHandler(Filters.contact, get_contact, pass_user_data=True))
+    dp.add_handler(MessageHandler(Filters.location, get_location, pass_user_data=True))
+
     dp.add_handler(MessageHandler(Filters.text, talk_to_me, pass_user_data=True))
 
     mybot.start_polling()
