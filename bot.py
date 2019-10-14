f80c0b1c595d4f51e43e12d074d07d65ec0b1335
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2019-10-14 13:23:43 +0200

    added anketa

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, RegexHandler
+from telegram.ext import Updater, CommandHandler, ConversationHandler, MessageHandler, Filters, RegexHandler
 
-from handlers import greet_user, send_cat_picture, get_contact, change_avatar, get_location, talk_to_me, check_user_photo
+from handlers import greet_user, send_cat_picture, get_contact, change_avatar, dontknow, get_location, talk_to_me, check_user_photo, anketa_get_name, anketa_start, anketa_rating, anketa_comment, anketa_skip_comment
 import settings
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
@@ -16,7 +16,20 @@ def main():
     logging.info('Бот запускается')
 
     dp = mybot.dispatcher
+
+    anketa = ConversationHandler(
+        entry_points=[RegexHandler('^(Заполнить анкету)$', anketa_start, pass_user_data=True)],
+        states={
+            "name": [MessageHandler(Filters.text, anketa_get_name, pass_user_data=True)],
+            "rating": [RegexHandler('^(1|2|3|4|5)$', anketa_rating, pass_user_data=True)],
+            "comment": [MessageHandler(Filters.text, anketa_comment, pass_user_data=True),
+                        CommandHandler('skip', anketa_skip_comment, pass_user_data=True)],
+        },
+        fallbacks=[MessageHandler(Filters.text | Filters.video | Filters.photo | Filters.document, dontknow, pass_user_data=True)]
+    )
+
     dp.add_handler(CommandHandler("start", greet_user, pass_user_data=True))
+    dp.add_handler(anketa)
     dp.add_handler(CommandHandler("cat", send_cat_picture, pass_user_data=True))
     dp.add_handler(RegexHandler('^(Прислать котика)$', send_cat_picture, pass_user_data=True))
     dp.add_handler(RegexHandler('^(Сменить аватарку)$', change_avatar, pass_user_data=True))
