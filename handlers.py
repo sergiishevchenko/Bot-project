9452b6fa106d526dcd0433ea97e4cdbf05fe8008
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2019-10-14 11:35:42 +0200

    added check_user_photo

--- handlers.py
+++ handlers.py
@@ -1,7 +1,8 @@
-from utils import get_keyboard, get_user_emo
+from utils import get_keyboard, get_user_emo, is_cat
 import logging
 from glob import glob
 from random import choice
+import os
 
 
 def greet_user(bot, update, user_data):
@@ -42,3 +43,18 @@ def get_contact(bot, update, user_data):
 def get_location(bot, update, user_data):
     print(update.message.location)
     update.message.reply_text('Спасибо {}'.format(get_user_emo(user_data)), reply_markup=get_keyboard())
+
+
+def check_user_photo(bot, update, user_data):
+    update.message.reply_text("Обрабатываю фото")
+    os.makedirs('downloads', exist_ok=True)
+    photo_file = bot.getFile(update.message.photo[-1].file_id)
+    filename = os.path.join('downloads', '{}.jpg'.format(photo_file.file_id))
+    photo_file.download(filename)
+    if is_cat(filename):
+        update.message.reply_text("Обнаружен котик, добавляю в библиотеку.")
+        new_filename = os.path.join('images', 'cat_{}.jpg'.format(photo_file.file_id))
+        os.rename(filename, new_filename)
+    else:
+        os.remove(filename)
+        update.message.reply_text("Тревога, котик не обнаружен!")
